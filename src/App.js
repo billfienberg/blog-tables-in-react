df8b57b26ab4207bbc818fe7829749fca3cccf15
Bill Fienberg <billfienberg@gmail.com>
2018-03-25 16:13:00 -0500

    Make tests pass

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React from 'react';
 import './App.css';
 
 const App = ({ states = {} }) => {
-  const tbodies = Object.values(states).map(state => <tbody>{state.name}</tbody>)
+  const stateValues = Object.values(states);
+  const tbodies = stateValues.map(state => {
+    const cityValues = Object.values(state.cities);
+    const cityRows = cityValues.map((city, i) => {
+      const stateName =
+        i === 0 ? <td rowSpan={cityValues.length + 1}>{state.name}</td> : null;
+      const stateAbbreviation =
+        i === 0 ? (
+          <td rowSpan={cityValues.length + 1}>{state.abbreviation}</td>
+        ) : null;
+      return (
+        <tr key={i}>
+          {stateName}
+          {stateAbbreviation}
+          <td>{city.name}</td>
+          <td>{city.metroPopulation}</td>
+        </tr>
+      );
+    });
+    return <tbody className={state.name}>{cityRows}</tbody>;
+  });
   return (
     <div>
       <table>
         <thead>
           <tr>
             <th colSpan="4">Metro Areas by State</th>
-          </tr>
-          <tr>
-            <th colSpan="2">State Info</th>
-            <th colSpan="2">City Info</th>
           </tr>
           <tr>
             <th>State Name</th>
