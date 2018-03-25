6d9b0782026b3caf3b48c6330a5cc4314ce1c3c5
Bill Fienberg <billfienberg@gmail.com>
2018-03-25 12:00:51 -0500

    Update setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
 configure({ adapter: new Adapter() });
+
+// https://github.com/facebook/jest/issues/3687#issuecomment-325373305
+Object.values = (obj) => Object.keys(obj).map(key => obj[key])
