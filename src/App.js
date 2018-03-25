4ae7bebf5034abbaac5ab4bceb2f2a9d8aef4fa6
Bill Fienberg <billfienberg@gmail.com>
2018-03-25 11:10:07 -0500

    Add hard-coded table

--- src/App.js
+++ src/App.js
@@ -1,21 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="4">Metro Areas by State</th>
+        </tr>
+        <tr>
+          <th colSpan="2">State Info</th>
+          <th colSpan="2">City Info</th>
+        </tr>
+        <tr>
+          <th>State Name</th>
+          <th>State Abbreviation</th>
+          <th>City</th>
+          <th>Population</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td rowSpan="7">Ohio</td>
+          <td rowSpan="7">OH</td>
+          <td>Cleveland</td>
+          <td>~2.1M</td>
+        </tr>
+        <tr>
+          <td>Columbus</td>
+          <td>~2.0M</td>
+        </tr>
+        <tr>
+          <td>Cincinnati</td>
+          <td>~2.1</td>
+        </tr>
+        <tr>
+          <td>Toledo</td>
+          <td>~600k</td>
+        </tr>
+        <tr>
+          <td>Dayton</td>
+          <td>~800k</td>
+        </tr>
+        <tr>
+          <td>Youngstown</td>
+          <td>~600k</td>
+        </tr>
+      </tbody>
+      <tbody>
+        <tr>
+          <td rowSpan="5">Texas</td>
+          <td rowSpan="5">TX</td>
+          <td>Austin</td>
+          <td>~2.1M</td>
+        </tr>
+        <tr>
+          <td>San Antonio</td>
+          <td>~2.5M</td>
+        </tr>
+        <tr>
+          <td>Houston</td>
+          <td>~6.3M</td>
+        </tr>
+        <tr>
+          <td>Dallas</td>
+          <td>~7.2M</td>
+        </tr>
+      </tbody>
+      <tbody>
+        <tr>
+          <td rowSpan="5">California</td>
+          <td rowSpan="5">CA</td>
+          <td>San Diego</td>
+          <td>~3.3M</td>
+        </tr>
+        <tr>
+          <td>Los Angeles</td>
+          <td>~13.1M</td>
+        </tr>
+        <tr>
+          <td>San Francisco</td>
+          <td>~4.7M</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
 
-export default App;
+export default App
