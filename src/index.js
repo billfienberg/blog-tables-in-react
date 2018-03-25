fc4d488ce11326e08c4da3ff38fb025bcb60801d
Bill Fienberg <billfienberg@gmail.com>
2018-03-25 16:14:32 -0500

    Add sample data

--- src/index.js
+++ src/index.js
@@ -4,5 +4,72 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const states = {
+  OH: {
+    name: 'Ohio',
+    abbreviation: 'OH',
+    cities: {
+      Cleveland: {
+        name: "Cleveland",
+        metroPopulation: "~2.1M"
+      },
+      Columbus: {
+        name: "Columbus",
+        metroPopulation: "~2.0M"
+      }
+    },
+  },
+  MA: {
+    name: 'Massachusetts',
+    abbreviation: 'MA',
+    cities: {
+      Boston: {
+        name: "Boston",
+        metroPopulation: "~4.6M"
+      }
+    }
+  },
+  TX: {
+    name: 'Texas',
+    abbreviation: 'TX',
+    cities: {
+      "Austin": {
+        name: "Austin",
+        metroPopulation: "~2.1M"
+      },
+      "San Antonio": {
+        name: "San Antonio",
+        metroPopulation: "~2.5M"
+      },
+      "Dallas": {
+        name: "Dallas",
+        metroPopulation: "~7.2M"
+      }
+    },
+  },
+  CA: {
+    name: 'California',
+    abbreviation: 'CA',
+    cities: {
+      "Los Angeles": {
+        name: "Los Angeles",
+        metroPopulation: "~13.1M"
+      },
+      "San Diego": {
+        name: "San Diego",
+        metroPopulation: "~3.3M"
+      },
+      "San Francisco": {
+        name: "San Francisco",
+        metroPopulation: "~4.7M"
+      },
+      "Sacramento": {
+        name: "Sacramento",
+        metroPopulation: "~2.1M"
+      }
+    },
+  },
+};
+
+ReactDOM.render(<App states={states} />, document.getElementById('root'));
 registerServiceWorker();
